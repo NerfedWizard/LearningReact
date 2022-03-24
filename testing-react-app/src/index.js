078e1e5ce4d52b69f43b0bc745091bc807595dbd
NerfedWizard <loelnelson@gmail.com>
2022-03-24 13:02:04 -0500

    Second Round

--- testing-react-app/src/index.js
+++ testing-react-app/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Author from './Author';
+import { useState } from 'react';
+// import Author from './Author';
+
+const PlusOne = () => {
+    const [counter, setCounter] = useState(0);
+    const increment = () => {
+        setCounter(counter + 1);
+        console.log(counter);
+    }
+    return <div className="App-header">
+        <div className="permanent-font">
+            <p>This is a counter Wahoo!!!!  _{counter}</p>
+            <button onClick={increment} > INCREMENT</button>
+        </div>
+    </div>
+}
 
 
-
-ReactDOM.render(<Author />, document.getElementById('root'));
+ReactDOM.render(<PlusOne />, document.getElementById('root'));
