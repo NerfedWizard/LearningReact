e15d7f3643cd3f93fffcd228546a1de636078725
NerfedWizard <loelnelson@gmail.com>
2022-03-24 20:40:59 -0500

    Quotes working

--- testing-react-app/src/index.js
+++ testing-react-app/src/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState } from 'react';
+// import axios from 'axios';
 // import Author from './Author';
 
 const PlusOne = () => {
     const [counter, setCounter] = useState(0);
+    const axios = require('axios');
+    const [data, setData] = useState([]);
+    const [quote, setQuote] = useState([]);
+    async function GetQuotes() {
+        increment();
+        const ident = "ID" + counter;
+        let result = await axios.get(`/api/quote/${ident}`);
+
+        setData(result.data);
+        console.log(ident);
+        setQuote(JSON.stringify(data, ['quoteAuthor', 'quoted', 'quoteCategory'], 4));
+        console.log(data);
+    }
     const increment = () => {
         setCounter(counter + 1);
         console.log(counter);
     }
     return <div className="App-header">
         <div className="permanent-font">
-            <p>This is a counter Wahoo!!!!  _{counter}</p>
-            <button onClick={increment} > INCREMENT</button>
+            <p>And Now I have quotes!!!</p>
+            <li>{quote}</li>
+            <button onClick={GetQuotes} > INCREMENT</button>
         </div>
     </div>
 }
+
 
 
 ReactDOM.render(<PlusOne />, document.getElementById('root'));
